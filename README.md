4d118cbf0e2e10a2be9294485ce251f40cb09eba
Raymond Oung <r.oung@hapi-robo.com>
2021-10-21 15:32:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android WebView Camera Example
+This is a minimal example of configuring camera permissions to work with Android's WebView.
+
+In this example, WebView loads [https://r-oung.github.io/webrtc-capturestill/](https://r-oung.github.io/webrtc-capturestill/).
+Source code for which can be found [here](https://github.com/r-oung/webrtc-capturestill).
---
+++ app/src/main/java/com/hrst/webview_webrtc/MainActivity.java
@@ -0,0 +1,79 @@
+package com.hrst.webview_webrtc;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+    final private String URL = "https://r-oung.github.io/webrtc-capturestill/";
+
+    @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webview);
+
+        // Initialize WebView
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                // Log JavaScript console messages
+                Log.d("Console", consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
+        });
+
+        // Check permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // All permissions have been granted
+            webview.loadUrl(URL);
+        } else {
+            // Ask for permissions
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA },0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 0) {
+            int sum = 0;
+            for (int result: grantResults) {
+                // result will be  0 if PackageManager.PERMISSION_GRANTED
+                // result will be -1 if PackageManager.PERMISSION_DENIED
+                sum += result;
+            }
+
+            if (grantResults.length > 0 && sum == 0) {
+                // All permissions have been granted
+                webview.loadUrl(URL);
+            } else {
+                Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
